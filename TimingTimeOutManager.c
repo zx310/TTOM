e75962ca5fdc71457b1be38b8e90783986f32a8d
zhouxin <31980744+zx310@users.noreply.github.com>
2022-05-07 13:45:01 +0800

    Add files via upload
    zhouxin:第一次提交代码

---
+++ TimingTimeOutManager.c
@@ -0,0 +1,184 @@
+/*
+ * TimingTimeOutManager.c
+ *
+ *  Created on: 2022年5月6日
+ *      Author: ZhouXin
+ */
+#include "TimingTimeOutManager.h"
+
+#define return_if_fail(p) if((p) == 0){printf ("[%s]:func error!\n", __func__);return;}
+
+static void* pthread_func (void* argv)
+{
+	static TTOMStatus_e status = TTOM_STOP;
+	TTOMPrivInfo_s *info = (TTOMPrivInfo_s*)argv;
+	unsigned int count = 0;
+
+	pthread_detach(pthread_self());
+    while (TTOM_TASK_RUNNING == info->task_running)
+    {
+    	//! An external API can control the completion of a task
+    	if(info->process_status == TTOM_STOP)
+    	{
+    		status = TTOM_STOP;
+    	}
+
+    	if(TTOM_STOP == status)
+    	{
+    		info->process_status = TTOM_STOP;
+			sem_wait(&info->s1);//阻塞在这里
+
+			count = info->time_out / info->timing;
+			//! count must greater than zero
+			status = (count > 0) ? TTOM_START : TTOM_STOP;
+			continue;
+    	}
+		else if(TTOM_START == status)
+		{
+			if(NULL != info->timing_call_back_func)
+			{
+				TTOMResult_e ret = info->timing_call_back_func(info->timing_call_back_argv);
+				//! If the expected result is obtained during scheduled invocation, the task is automatically terminated
+				if(TTOM_SUEECSS == ret)
+				{
+					status = TTOM_STOP;
+					if(NULL != info->success_call_back_func)
+					{
+						info->success_call_back_func(info->success_call_back_argv);
+					}
+					continue;
+				}
+			}
+
+			count--;
+			//! If the expected result is not obtained during the scheduled call until the timeout, perform the following steps
+			if(count == 0)
+			{
+				status = TTOM_STOP;
+				if(NULL != info->time_out_call_back_func)
+				{
+					info->time_out_call_back_func(info->time_out_call_back_argv);
+				}
+				continue;
+			}
+    	}
+        sleep (info->timing);
+    }
+
+    pthread_exit(NULL);
+}
+
+void TTOM_Init(TTOMPrivInfo_s* info,  unsigned int time_out, unsigned int timing)
+{
+	return_if_fail (info != NULL);
+	pthread_t pt = 0;
+	int ret = 0;
+
+	if(info->task_running != TTOM_TASK_RUNNING)
+	{
+		info->task_running = TTOM_TASK_RUNNING;
+		info->process_status = TTOM_STOP;
+		info->time_out = time_out;
+		info->timing = timing;
+		info->timing_call_back_func = NULL;
+		info->time_out_call_back_func = NULL;
+		info->success_call_back_func = NULL;
+		info->timing_call_back_argv = NULL;
+		info->time_out_call_back_argv = NULL;
+		info->success_call_back_argv = NULL;
+		sem_init (&info->s1, 0, 0);
+
+		ret = pthread_create (&pt, NULL, (void*)pthread_func, (void*)info);
+		if (ret != 0)
+		{
+			info->task_running = !TTOM_TASK_RUNNING;
+			sem_destroy(&info->s1);
+			printf("%s: pthread_create error\n", __func__);
+		}
+	}
+}
+
+void TTOM_SetTimingCallBackFunc(TTOMPrivInfo_s* info, TTOMCallBack timing_call_back_func, void* timing_call_back_argv)
+{
+	return_if_fail (info != NULL);
+	if(info->process_status != TTOM_START)
+	{
+		info->timing_call_back_func = timing_call_back_func;
+		info->timing_call_back_argv = timing_call_back_argv;
+	}
+}
+
+void TTOM_SetSuccessCallBackFunc(TTOMPrivInfo_s* info, TTOMCallBack success_call_back_func, void* success_call_back_argv)
+{
+	return_if_fail (info != NULL);
+		if(info->process_status != TTOM_START)
+		{
+			info->success_call_back_func = success_call_back_func;
+			info->success_call_back_argv = success_call_back_argv;
+		}
+}
+void TTOM_SetTimeOutCallBackFunc(TTOMPrivInfo_s* info, TTOMCallBack time_out_call_back_func, void* time_out_call_back_argv)
+{
+	return_if_fail (info != NULL);
+	if(info->process_status != TTOM_START)
+	{
+		info->time_out_call_back_func = time_out_call_back_func;
+		info->time_out_call_back_argv = time_out_call_back_argv;
+	}
+}
+
+void TTOM_DeInit(TTOMPrivInfo_s* info)
+{
+	return_if_fail (info != NULL);
+	info->task_running = !TTOM_TASK_RUNNING;
+	info->timing_call_back_func = NULL;
+	info->time_out_call_back_func = NULL;
+	info->timing_call_back_argv = NULL;
+	info->time_out_call_back_argv = NULL;
+	info->success_call_back_func = NULL;
+	info->success_call_back_argv = NULL;
+	sem_destroy(&info->s1);
+	if(info->process_status != TTOM_STOP)
+	{
+		info->process_status = TTOM_STOP;
+	}
+}
+
+void TTOM_Stop(TTOMPrivInfo_s* info)
+{
+	return_if_fail (info != NULL);
+	info->process_status = TTOM_STOP;
+}
+
+void TTOM_Start(TTOMPrivInfo_s* info)
+{
+	return_if_fail (info != NULL);
+	if(info->process_status != TTOM_START)
+	{
+		info->process_status = TTOM_START;
+		sem_post(&info->s1);
+	}
+}
+
+void TTOM_Start2(TTOMPrivInfo_s* info, unsigned int time_out, unsigned int timing)
+{
+	return_if_fail (info != NULL);
+	if(info->process_status != TTOM_START)
+	{
+		info->time_out = time_out;
+		info->timing = timing;
+		info->process_status = TTOM_START;
+		sem_post(&info->s1);
+	}
+}
+
+
+TTOMStatus_e TTOM_Get_Status(TTOMPrivInfo_s* info)
+{
+	if(NULL == info)
+	{
+		return TTOM_STOP;
+	}
+	return info->process_status;
+}
+
---
+++ TimingTimeOutManager.h
@@ -0,0 +1,165 @@
+/*
+ * TimingTimeOutManager.h
+ *
+ *  Created on: 2022年5月6日
+ *      Author: ZhouXin
+ */
+
+#ifndef TIMINGTIMEOUTMANAGER_H_
+#define TIMINGTIMEOUTMANAGER_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define TTOM_TASK_RUNNING	(1)
+
+typedef enum _TaskStatus
+{
+	TTOM_STOP = 0,
+	TTOM_START,
+}TTOMStatus_e;
+
+typedef enum _TaskResult
+{
+	TTOM_FAILED = 0,
+	TTOM_SUEECSS = 1,
+}TTOMResult_e;
+
+typedef TTOMResult_e (*TTOMCallBack)(void*);
+
+typedef struct _PrivInfo
+{
+	TTOMStatus_e process_status;   //task status
+    unsigned int task_running;		//task thread running or not
+    unsigned int time_out;			//task timeout time
+    unsigned int timing;			//task timing time
+    sem_t s1;						//task semaphore
+    TTOMCallBack timing_call_back_func;   //timing task function
+    void* timing_call_back_argv;	   	  //timing task function params
+    TTOMCallBack success_call_back_func;   //timing task sucess function
+    void* success_call_back_argv;	      //timing task function sucess params
+    TTOMCallBack time_out_call_back_func;  //timeout call back function
+    void* time_out_call_back_argv;	   //timeout call back function params
+}TTOMPrivInfo_s;
+
+/**
+ * @fn void TTOM_Init(TTOMPrivInfo_s*, unsigned int, unsigned int)
+ * @brief
+ * 		  initialization  TTOM
+ * @param info		TTOMPrivInfo_s struct pointer
+ * @param time_out	timeout time, unit: s
+ * @param timing	timing time: unit: s
+ */
+void TTOM_Init(TTOMPrivInfo_s* info,  unsigned int time_out, unsigned int timing);
+
+/**
+ * @fn void TTOM_SetTimingCallBackFunc(TTOMPrivInfo_s*, TTOMCallBack,)
+ * @brief
+ * 		  setting timing time out callback function,  must be called before TTOM_Start
+ * @param info	TTOMPrivInfo_s struct pointer
+ * @param timing_call_back_func  timing time out call back function pointer
+ * @param timing_call_back_argv	 timing time out call back function parameter
+ */
+void TTOM_SetTimingCallBackFunc(TTOMPrivInfo_s* info, TTOMCallBack timing_call_back_func, void* timing_call_back_argv);
+/**
+ * @fn void TTOM_SetSuccessCallBackFunc(TTOMPrivInfo_s*, TTOMCallBack,)
+ * @brief
+ * 		  setting timing task successful callback function,  must be called before TTOM_Start
+ * @param info	TTOMPrivInfo_s struct pointer
+ * @param success_call_back_func  timing task successful call back function pointer
+ * @param success_call_back_argv	 timing task successful call back function parameter
+ */
+void TTOM_SetSuccessCallBackFunc(TTOMPrivInfo_s* info, TTOMCallBack success_call_back_func, void* success_call_back_argv);
+/**
+ * @fn void TTOM_SetTimeOutCallBackFunc(TTOMPrivInfo_s*, TTOMCallBack)
+ * @brief
+ * 		  setting time out callback function,  must be called before TTOM_Start
+ * @param info	TTOMPrivInfo_s struct pointer
+ * @param time_out_call_back_func 	time out call back function pointer
+ * @param time_out_call_back_argv	time out call back function pointer
+ */
+void TTOM_SetTimeOutCallBackFunc(TTOMPrivInfo_s* info, TTOMCallBack time_out_call_back_func, void* time_out_call_back_argv);
+/**
+ * @fn void TTOM_DeInit(TTOMPrivInfo_s*)
+ * @brief
+ * 		  uninitialization TTOM, It is best to call TTOM_Stop before calling this function.
+ * @param info	TTOMPrivInfo_s struct pointer
+ */
+void TTOM_DeInit(TTOMPrivInfo_s* info);
+/**
+ * @fn void TTOM_Stop(TTOMPrivInfo_s*)
+ * @brief
+ *		  stop TTOM
+ * @param info	TTOMPrivInfo_s struct pointer
+ */
+void TTOM_Stop(TTOMPrivInfo_s* info);
+/**
+ * @fn void TTOM_Start(TTOMPrivInfo_s*)
+ * @brief
+ * 		  start TTOM, use initialization function timeout time and timing time
+ * @param info	TTOMPrivInfo_s struct pointer
+ */
+void TTOM_Start(TTOMPrivInfo_s* info);
+/**
+ * @fn void TTOM_Start2(TTOMPrivInfo_s*, unsigned int, unsigned int)
+ * @brief
+ * 		  start TTOM, use newer timeout time and timing time
+ * @param info		TTOMPrivInfo_s struct pointer
+ * @param time_out	timeout time, unit: s
+ * @param timing	timing time: unit: s
+ */
+void TTOM_Start2(TTOMPrivInfo_s* info, unsigned int time_out, unsigned int timing);
+/**
+ * @fn TTOTMStatus_e TTOM_Get_Status(TTOMPrivInfo_s*)
+ * @brief
+ * 		  get task status, stop or start
+ * @param info	TTOMPrivInfo_s struct pointer
+ * @return
+ */
+TTOMStatus_e TTOM_Get_Status(TTOMPrivInfo_s* info);
+
+
+/*example test code*/
+#if 0
+#include "TimingTimeOutManager.h"
+#include <stdio.h>
+#include <string.h>
+
+#define TIME_OUT	(10) //s
+#define TIMING		(1) //s
+
+static void timing_call_back_func(void* argv)
+{
+	int tmp = *(int*)argv;
+	printf("hello [%d]\n", tmp);
+}
+static void timeout_call_back_func(void* argv)
+{
+	int tmp = *(int*)argv;
+	printf("timeout [%d]\n", tmp);
+}
+
+int main(void)
+{
+	setbuf(stdout,NULL);
+	TTOMPrivInfo_s info;
+	int tmp = 100;
+	int tmp1 = 1000;
+	memset(&info, 0, sizeof(TTOMPrivInfo_s));
+
+	TTOM_Init(&info, TIME_OUT, TIMING);
+	TTOM_SetTimingCallBackFunc(&info, timing_call_back_func,  (void*)&tmp);
+	TTOM_SetTimeOutCallBackFunc(&info, timeout_call_back_func,  (void*)&tmp1);
+	sleep(5);
+	TTOM_Start(&info);
+	sleep(TIME_OUT+1);
+
+	return EXIT_SUCCESS;
+}
+#endif
+
+
+#endif /* TIMINGTIMEOUTMANAGER_H_ */
